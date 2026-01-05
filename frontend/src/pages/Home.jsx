c8001c73cdda9e2d802c498698aea860d910ca44
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-01-05 17:38:41 +1300

    First Frontend commmit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css"
+function Home(){
+    return(
+       <div className="home-container">
+        <h1 className="title">Tempus Arcanum</h1>
+        <h3 className="quote">"Within these shadowed halls, thy secrets shall slumber—
+sealed by wax and bound by time, until the appointed hour of awakening."</h3>
+<div className="buttons">
+    <button className="login">Login</button>
+    <button className='signup'>Sign Up</button>
+</div>
+        </div>
+    )
+}
+export default Home;
